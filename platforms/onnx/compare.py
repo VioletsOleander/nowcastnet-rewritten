97c8abb220d265261d940d46b3a8de3194c00ac2
VioletsOleander <1377232072@qq.com>
2025-04-10 11:58:39 +0000

    feat: add ONNX export support

---
+++ platforms/onnx/compare.py
@@ -0,0 +1,70 @@
+"""Compare the evaluation result of ONNX model with Torch model."""
+
+import os
+
+from utils import setup_parser, load_config, construct_configs
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_lines(x,  y1, y2, title, x_label, y_label, x_ticks, y_ticks, save_dir) -> None:
+    fig = plt.figure()
+    ax = plt.axes()
+
+    ax.plot(x, y1, label='ONNX')
+    ax.plot(x, y2, label='Torch')
+
+    ax.set_title(title)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    ax.set_xticks(x_ticks)
+    ax.set_yticks(y_ticks)
+    ax.legend()
+
+    plt.savefig(os.path.join(save_dir, title + '.png'), dpi=300)
+    plt.close()
+
+
+if __name__ == "__main__":
+    parser = setup_parser(
+        'Compare the evaluation result of ONNX model with Torch model')
+    args = parser.parse_args()
+
+    configs = load_config(args.config_path).value
+    configs = {key: value + args.case_type
+               for key, value in configs.items()}
+    configs = construct_configs(configs)
+
+    os.makedirs(configs.save_dir, exist_ok=True)
+
+    print('Comparing ONNX and Torch evaluation results')
+    onnx_numpy_files = sorted([filename for filename in os.listdir(configs.onnx_eval_results_path)
+                               if filename.endswith('.npy')])
+    torch_numpy_files = sorted([filename for filename in os.listdir(configs.torch_eval_results_path)
+                                if filename.endswith('.npy')])
+
+    for onnx_numpy_file, torch_numpy_file in zip(onnx_numpy_files, torch_numpy_files):
+        print('Comparing', onnx_numpy_file, torch_numpy_file)
+        onnx_numpy = np.load(os.path.join(
+            configs.onnx_eval_results_path, onnx_numpy_file))
+        torch_numpy = np.load(os.path.join(
+            configs.torch_eval_results_path, torch_numpy_file))
+
+        title = onnx_numpy_file.split('.')[0].upper()
+        if 'CSI' in title:
+            y_label = 'CSI'
+        else:
+            y_label = 'CSIN'
+
+        plot_lines(x=np.arange(0, len(onnx_numpy)),
+                   y1=onnx_numpy,
+                   y2=torch_numpy,
+                   title=title,
+                   x_label='Prediction Interval (10 min)',
+                   y_label=y_label,
+                   x_ticks=range(0, len(onnx_numpy), 3),
+                   y_ticks=np.arange(0, 1, 0.2),
+                   save_dir=configs.save_dir)
+        print('Comparison saved to', configs.save_dir)
+    print('Comparison finished')
---
+++ platforms/onnx/inference.py
@@ -0,0 +1,80 @@
+"""Do ONNX model Inference."""
+
+import logging
+import os
+
+from utils import setup_parser, load_config, construct_configs
+
+import onnxruntime as ort
+import numpy as np
+from torch.utils.data import DataLoader
+from nowcastnet.utils.logging import setup_logging, log_configs
+from nowcastnet.utils.visualizing import crop_frames, plot_frames
+from nowcastnet.datasets.factory import dataset_provider
+
+
+def inference(sess: ort.InferenceSession, dataloader: DataLoader, configs):
+    logging.info('Inference started')
+
+    np.random.seed(configs.seed)
+
+    input_name = ort_session.get_inputs()[0].name
+    noise_name = ort_session.get_inputs()[1].name
+    output_name = ort_session.get_outputs()[0].name
+
+    results_dir = configs.results_path
+    os.makedirs(results_dir, exist_ok=True)
+
+    for batch, (observed_frames, _) in enumerate(dataloader):
+        logging.info(f'Batch: {batch+1}/{len(dataloader)}')
+
+        observed_frames = observed_frames.numpy()
+        noise = np.random.rand(
+            configs.batch_size,
+            configs.generator_base_channels,
+            configs.image_height//32,
+            configs.image_width//32).astype(np.float32)
+
+        predicted_frames = ort_session.run(
+            [output_name], {input_name: observed_frames, noise_name: noise})[0]
+
+        result_path = os.path.join(results_dir, str(batch))
+        os.makedirs(result_path, exist_ok=True)
+
+        if configs.case_type == 'normal':
+            predicted_frames = crop_frames(frames=predicted_frames,
+                                           crop_size=configs.crop_size)
+
+        plot_frames(frames=predicted_frames[0],
+                    save_dir=result_path,
+                    vmin=1,
+                    vmax=40)
+
+        if configs.save_original_data:
+            np.save(os.path.join(result_path, 'frames.npy'),
+                    predicted_frames[0])
+
+    logging.info('Inference finished')
+    logging.info(f'Results saved to {results_dir}')
+
+
+if __name__ == "__main__":
+    parser = setup_parser('Do ONNX model Inference')
+    args = parser.parse_args()
+
+    configs = load_config(args.config_path).value
+    configs = construct_configs(configs, ['common', args.case_type])
+    configs.__setattr__('case_type', args.case_type)
+
+    os.makedirs(os.path.dirname(configs.path_to_log), exist_ok=True)
+    setup_logging(configs.path_to_log)
+    log_configs(configs)
+
+    dataloader = dataset_provider(configs)
+    logging.info(f'DataLoader created from {configs.dataset_path}')
+
+    ort_session = ort.InferenceSession(
+        configs.graph_path, providers=['CPUExecutionProvider'])
+    logging.info(f'ORT inference session created from {configs.graph_path}')
+
+    inference(ort_session, dataloader, configs)
